7c1785f65add60c74a24754b74b514d34d5ab993
Abhijay Tyagi <abhijay@pop-os.localdomain>
2024-07-10 01:21:26 +0530

    made random to-do-list

--- to-do-list/src/index.ts
+++ to-do-list/src/index.ts
@@ -1,11 +1,59 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuid} from "uuid"
 
-import confetti from 'canvas-confetti';
+type task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const loadTasks = (): task[] => {
+  const JSONString = localStorage.getItem("TASKS")
+  if(!JSONString) return []
+  return JSON.parse(JSONString)
+}
+
+const addListItem = (task: task) => {
+  const newItem = document.createElement('li')
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = 'checkbox'
+  label.append(checkbox,task.title)
+  newItem.append(label)
+  list?.append(newItem)
+
+}
+
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const taskForm = document.querySelector<HTMLFormElement>("#new-task-form") 
+const taskTitle = document.querySelector<HTMLInputElement>("#new-task-title")
+
+const tasks: task[] = loadTasks()
+tasks.forEach(addListItem)
+
+taskForm?.addEventListener("submit" , e => {
+  e.preventDefault()
+  if(taskTitle?.value == "" || taskTitle?.value == null) return
+
+  const newTask: task = {
+    id: uuid(),
+    title: taskTitle.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(newTask)
+
+  addListItem(newTask)
+  taskTitle.value = ""
+})
+
+
+const saveTasks = () => {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
